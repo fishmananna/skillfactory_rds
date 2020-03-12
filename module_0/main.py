435fe3532e89bcca15d77fd51ad98dd5506b25ef
fishmananna <annnetfish@gmail.com>
2020-03-12 13:07:44 +0300

    Project

--- module_0/main.py
+++ module_0/main.py
@@ -1,1 +1,14 @@
-print("Hello world")
+import numpy as np
+def my_programm(number):
+    count = 0
+    left = 1
+    right = 100
+    predict = (right + left) // 2
+    while number != predict:
+        count+=1
+        if number > predict: 
+            left = predict + 1
+        elif number < predict: 
+            right = predict - 1
+        predict = (right + left) // 2
+    return(count)
